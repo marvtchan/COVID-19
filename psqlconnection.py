56b8006bbb7e7f56e93736c4997c48ef8a789f2c
Marvin Chan <49408616+marvtchan@users.noreply.github.com>
2021-09-25 16:34:59 -0700

    Delete psqlconnection.py

--- psqlconnection.py
+++
@@ -1,5 +0,0 @@
-#psqlconnection.py
-
-DATABASE_URL = 'postgres://wbednsipiaqkbj:d5e3da30cfbde8bc3d815da1e55ff3151702373e3771043efd7d29377f2eb4d8@ec2-34-200-158-205.compute-1.amazonaws.com:5432/d4gb3iqvaak9j1'
-
-# postgresql://marvinchan:shadow8@localhost:5432/covid
